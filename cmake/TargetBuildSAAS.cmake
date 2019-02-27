f7cc4c8b1a9b7d34e9638415d4881cfc5a74176d
lcausin <test@test.com>
2019-02-27 14:05:33 +0100

    Automatic project creation

--- cmake/TargetBuildSAAS.cmake
+++ cmake/TargetBuildSAAS.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.2.1)
 
-find_program(SHGAMEPACKAGER_EXECUTABLE ShGamePackager HINTS ${SHINE_TOOLS_DIR} "$ENV{SHINE_BASE_DIR}/bin/tools/" PATHS /usr/local/
+find_program(SHGAMEPACKAGER_EXECUTABLE ShGamePackager HINTS "${SHINE_TOOLS_DIR}" "$ENV{SHINE_BASE_DIR}" PATHS "/usr/local/" PATH_SUFFIXES "" "bin/tools" "bin/Tools" "Bin/tools" "/Bin/Tools"
 	NO_DEFAULT_PATH
 	NO_CMAKE_ENVIRONMENT_PATH
 	NO_CMAKE_PATH
